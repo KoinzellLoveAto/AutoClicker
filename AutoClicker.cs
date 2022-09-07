668fe48f86720319800c549884ec2f65a4374f1b
GISBERT Baptiste <46565128+KoinzellLoveAto@users.noreply.github.com>
2022-09-07 15:54:10 +0200

    test

---
+++ AutoClicker.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace testAutoclicker
+{
+    class AutoClicker
+    {
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
+
+
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+
+        //mouse controls
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+
+        //== Delegate ==/
+        // Invoke when simulate click
+        Action clickEvent;
+
+        // reference to the UI
+        private MainWindow MainWindow;
+
+
+        public bool isRunning;
+        Thread threadAutoClicker;
+
+        public int counter = 0; // number of click
+        public int delay = 500; // delay betwenn 2 click 
+
+        public int delayInaccuracy = 0; // delay error in %
+        public int positionInaccuracy = 0;
+
+        //Screen Axis
+        struct Axis
+        {
+            public int x;
+            public int y;
+        }
+
+        //Constructor 
+        public AutoClicker(MainWindow MainWindow)
+        {
+            this.MainWindow = MainWindow;
+            MainWindow.onPressF6 += Start;
+            clickEvent += Click;
+
+        }
+
+        //Destructor
+        ~AutoClicker()
+        {
+            clickEvent -= Click;
+        }
+
+        //This function is called when we START the autoclicker
+        public void Start()
+        {
+            // switch subscribe
+            MainWindow.onPressF6 -= Start;
+            MainWindow.onPressF6 += Stop;
+
+            counter = 0;
+            MainWindow.StateAutoclicker.Text = "Press F6 to Stop !";
+
+            isRunning = true;
+            CalculDelay();
+
+            delayInaccuracy = Int32.Parse(MainWindow.Freq.Text);
+            positionInaccuracy = Int32.Parse(MainWindow.Position.Text);
+
+
+            threadAutoClicker = new Thread(Run); // create a thread to run without lock
+            threadAutoClicker.Start();
+        }
+
+        //This function is called when we STOP the autoclicker
+        public void Stop()
+        {
+            // switch subscribe
+            MainWindow.onPressF6 -= Stop;
+            MainWindow.onPressF6 += Start;
+
+            MainWindow.StateAutoclicker.Text = "Press F6 to Run !";
+
+            UpdateUI();
+            isRunning = false;
+        }
+
+
+        // LOOP when autoclicker is active
+        public void Run()
+        {
+            while (isRunning && threadAutoClicker.IsAlive)
+            {
+                Click();
+                Thread.Sleep(GetInaccuracyDelay(delay));
+            }
+
+
+        }
+
+
+        private void Click()
+        {
+            MainWindow.Dispatcher.BeginInvoke(() =>
+            {
+                if (isRunning)
+                {
+                    MouseLeftClick();
+                    UpdateUI();
+
+                }
+            });
+        }
+
+
+
+        private void UpdateUI()
+        {
+            MainWindow.numberOfClick.Text = counter.ToString();
+        }
+
+        public int GetInaccuracyDelay(int initialDelay)
+        {
+            if (delayInaccuracy == 0) return initialDelay;
+
+            Random rnd = new Random();
+            int finalDelay;
+            int coeff = (delayInaccuracy / 100);
+
+            int min = initialDelay * (1 - coeff);
+            if (min <= 0) min = 0;
+
+            int max = initialDelay * (1 + coeff);
+
+            finalDelay = rnd.Next(rnd.Next(min, max));
+
+            return finalDelay;
+        }
+
+        private void MouseLeftClick()
+        {
+            Axis tmp = GetMousePosition();
+            int finalX = tmp.x;
+            int finalY = tmp.y;
+
+            if (positionInaccuracy != 0)
+            {
+
+                int coeff = (positionInaccuracy / 100);
+                
+
+                Random rnd = new Random();
+                int min = tmp.x * (1 - coeff);
+                if (min <= 0) min = 0;
+
+                int max = tmp.x * (1 + coeff);
+
+                finalX = rnd.Next(rnd.Next(min, max));
+
+
+                min = tmp.y * (1 - coeff);
+                if (min <= 0) min = 0;
+
+                max = tmp.y * (1 + coeff);
+
+                finalY = rnd.Next(rnd.Next(min, max));
+
+            }
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            counter++;
+        }
+
+
+        //get the mouse position from the user32.dll then cast to INT
+        private static  Axis GetMousePosition()
+        {
+            Axis axis;
+            Point pt = new Point();
+            GetCursorPos(ref pt);
+            axis.x = (int)pt.X;
+            axis.y = (int)pt.Y;
+            return axis;
+        }
+
+        //return delay in ms
+        public void CalculDelay()
+        {
+            int totalDelay = 0;
+            int millisecond;
+            int second;
+            int minute;
+
+
+            if (MainWindow.MilliSecondField.Text == "")
+                millisecond = 0;
+            else
+                millisecond = Int32.Parse(MainWindow.MilliSecondField.Text);
+
+
+            if (MainWindow.SecondField.Text == "")
+                second = 0;
+            else
+                second = Int32.Parse(MainWindow.SecondField.Text);
+
+
+            if (MainWindow.MinuteField.Text == "")
+                minute = 0;
+            else
+                minute = Int32.Parse(MainWindow.MinuteField.Text);
+
+            totalDelay += millisecond;
+            totalDelay += second * 1000;
+            totalDelay += minute * 1000 * 60;
+
+            delay = totalDelay;
+
+            if (delay < 5)
+            {
+                delay = 5;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+
+namespace testAutoclicker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // IMPORTATIONS des fonctions voulu.
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+
+        // define  hotkey & key
+        private const int HOTKEY_ID = 9000;
+
+        //Modifiers:
+        private const uint MOD_NONE = 0x0000; //(none)
+        private const uint MOD_ALT = 0x0001; //ALT
+        private const uint MOD_CONTROL = 0x0002; //CTRL
+        private const uint MOD_SHIFT = 0x0004; //SHIFT
+        private const uint MOD_WIN = 0x0008; //WINDOWS
+        //CAPS LOCK:
+
+        private const uint VK_F6 = 0x75; //F6
+        private const uint VK_CAPITAL = 0x14;
+
+
+        // attribut
+        private IntPtr _windowHandle;
+        private HwndSource _source;
+        private AutoClicker autoClicker;
+
+
+        // event
+        public Action onCapsLock;
+        public Action onPressF6;
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            autoClicker = new AutoClicker(this);
+          
+        }
+
+
+      
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        //override to unregister HOTKEY listener
+        protected override void OnClosed(EventArgs e)
+        {
+            _source.RemoveHook(HwndHook);
+            UnregisterHotKey(_windowHandle, HOTKEY_ID);
+            base.OnClosed(e);
+        }
+
+        //override to register HOTKEY listener
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            _windowHandle = new WindowInteropHelper(this).Handle;
+            _source = HwndSource.FromHwnd(_windowHandle);
+            _source.AddHook(HwndHook);
+
+            RegisterHotKey(_windowHandle, HOTKEY_ID, MOD_CONTROL, VK_CAPITAL); //CTRL + CAPS_LOCK
+            RegisterHotKey(_windowHandle, HOTKEY_ID, MOD_NONE, VK_F6); //NONE + F6
+
+        }
+
+        // HOTKEY listener
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_HOTKEY = 0x0312;
+
+            switch (msg)
+            {
+                case WM_HOTKEY:
+
+                    switch (wParam.ToInt32())
+                    {
+                        case HOTKEY_ID:
+                            int vkey = (((int)lParam >> 16) & 0xFFFF);
+                            if (vkey == VK_CAPITAL)
+                            {
+                                onCapsLock?.Invoke();
+
+                            }
+                            if (vkey == VK_F6)
+                            {
+                                onPressF6?.Invoke();
+                            }
+                            handled = true;
+                            break;
+                    }
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+
+        private void OnLostFocus_numericField(object sender, RoutedEventArgs e)
+        {
+            //get the source form the event
+            TextBox textBox = (TextBox)e.Source;
+
+            //if the field is empty we set 0
+            if (textBox.Text == "")
+                textBox.Text = "0";
+
+
+        }
+
+        private void OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            //get the source form the event
+            TextBox textBox = (TextBox)e.Source;
+
+            //if the field is 0 we set empty
+            if (textBox.Text == "0")
+                textBox.Text = "";
+
+
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void Btn_minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Btn_Close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
